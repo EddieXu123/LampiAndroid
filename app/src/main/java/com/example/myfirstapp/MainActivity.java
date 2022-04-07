4f69be543a7d173cf1da1b57929836341f028de0
Eddie Xu <erx@case.edu>
2022-04-07 14:19:02 -0400

    Hue and Saturation work

--- app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -1,13 +1,20 @@
 package com.example.myfirstapp;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.Resources;
+import android.graphics.Canvas;
 import android.graphics.Color;
-import android.graphics.LinearGradient;
-import android.graphics.Shader;
+import android.graphics.ColorFilter;
+import android.graphics.Paint;
+import android.graphics.PixelFormat;
+import android.graphics.Rect;
+import android.graphics.RectF;
 import android.graphics.drawable.Drawable;
-import android.graphics.drawable.ShapeDrawable;
-import android.graphics.drawable.shapes.RectShape;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -15,63 +22,55 @@ import android.widget.ImageButton;
 import android.widget.SeekBar;
 
 import com.rtugeek.android.colorseekbar.ColorSeekBar;
-import com.rtugeek.android.colorseekbar.OnColorChangeListener;
-import com.rtugeek.android.colorseekbar.thumb.DefaultThumbDrawer;
 
 public class MainActivity extends AppCompatActivity {
 
     View view;
     ColorSeekBar colorSeekBar;
-    ColorSeekBar alphaSeekBar;
+    ColorSeekBar saturation_seekbar;
+    ColorSeekBar brightness_seekbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ImageButton button = (ImageButton) this.findViewById(R.id.imageButton);
-        button.setColorFilter(Color.argb(255, 0, 0, 255)); // White Tint
-
+        button.setColorFilter(Color.argb(255, 0, 0, 255)); // Blue Tint
+        button.bringToFront();
         view = findViewById(R.id.view);
 
-        colorSeekBar = findViewById(R.id.color_seek_bar);
-        colorSeekBar.setColor(Color.RED);
-        alphaSeekBar = findViewById(R.id.alpha_seek_bar);
-        view.setBackgroundColor(colorSeekBar.getColor());
 
-//        Log.d("GettingColor", Integer.toString(colorSeekBar.getColor()));
 
-//        alphaSeekBar.setColorSeeds(new int[]{Color.WHITE, Color.RED});
+        float[] hsv = new float[3];
+//        Color.RGBToHSV(red, green, blue, hsv);
+
 
         seekBarCreation();
 
-        Log.d("Startup", "STARTED");
     }
 
 
     public void seekBarCreation() {
-        colorSeekBar.setOnColorChangeListener((progress, color) -> {
-            alphaSeekBar.setColorSeeds(new int[]{Color.WHITE, colorSeekBar.getColor()});
-            view.setBackgroundColor(color);
+
+        colorSeekBar = findViewById(R.id.color_seek_bar);
+        saturation_seekbar = findViewById((R.id.saturation_seekbar));
+        brightness_seekbar = findViewById((R.id.brightness_seekbar));
+
+        // i is hue (outa 100), i1 is saturation (out of 255) i2 color
+        colorSeekBar.setOnColorChangeListener((colorBarPosition, alphaBarPosition, color) -> {
+            saturation_seekbar.setColorSeeds(new int[]{Color.WHITE, colorSeekBar.getColor()});
+            view.setBackgroundColor(saturation_seekbar.getColor());
+            Log.d("Color Change", Integer.toString(colorBarPosition));
         });
 
-        alphaSeekBar.setOnColorChangeListener((progress, color) -> {
-            Log.d("Current Color", Integer.toString(alphaSeekBar.getColor()));
-            view.setBackgroundColor(alphaSeekBar.getColor());
+        saturation_seekbar.setOnColorChangeListener((colorBarPosition, alphaBarPosition, color) -> {
+            view.setBackgroundColor(color);
+            Log.d("Saturation Change", Integer.toString(colorBarPosition));
+        });
+
+        brightness_seekbar.setOnColorChangeListener((colorBarPosition, alphaBarPosition, color) -> {
+//            view.(saturation_seekbar.getColor());
+            Log.d("Brightness Change", Integer.toString(color));
         });
     }
-
-//    public void seekBarCreation() {
-//        LinearGradient test = new LinearGradient(0.f, 0.f, 300.f, 0.0f,
-//
-//                new int[]{0xFF000000, 0xFF0000FF, 0xFF00FF00, 0xFF00FFFF,
-//                        0xFFFF0000, 0xFFFF00FF, 0xFFFFFF00, 0xFFFFFFFF},
-//                null, Shader.TileMode.CLAMP);
-//        ShapeDrawable shape = new ShapeDrawable(new RectShape());
-//        shape.getPaint().setShader(test);
-//
-//        SeekBar seekBarFont = (SeekBar) findViewById(R.id.seekbar_font);
-//        seekBarFont.setProgressDrawable((Drawable) shape);
-//
-//    }
-
 }
