abca41a01236404ad758e2c6fae36d1bc753a257
Eddie Xu <erx@case.edu>
2022-04-26 15:40:20 -0400

    Cleanup for bluetooth

--- app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -1,160 +1,61 @@
 package com.example.myfirstapp;
 
-import static android.bluetooth.BluetoothGatt.CONNECTION_PRIORITY_HIGH;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.annotation.RequiresApi;
-
-import android.bluetooth.BluetoothGatt;
-import android.graphics.BlendMode;
-import android.graphics.BlendModeColorFilter;
-
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.jakewharton.rx.transformer.ReplayingShare;
-
-import android.graphics.LinearGradient;
-import android.graphics.PorterDuffColorFilter;
-import android.graphics.Shader;
-import android.graphics.drawable.Drawable;
-import android.graphics.drawable.GradientDrawable.Orientation;
-import android.graphics.drawable.Drawable;
-import android.graphics.drawable.GradientDrawable;
-
-import androidx.appcompat.content.res.AppCompatResources;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
-import androidx.core.graphics.drawable.DrawableCompat;
-import androidx.lifecycle.Observer;
-
-import android.Manifest;
-import android.accessibilityservice.AccessibilityService;
-import android.annotation.SuppressLint;
-import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
-import android.bluetooth.BluetoothDevice;
-
-import static android.bluetooth.BluetoothGatt.CONNECTION_PRIORITY_HIGH;
-
-import android.bluetooth.BluetoothGattCharacteristic;
-import android.bluetooth.BluetoothManager;
-import android.bluetooth.le.BluetoothLeScanner;
-import android.bluetooth.le.ScanCallback;
-import android.bluetooth.le.ScanResult;
-import android.companion.AssociationRequest;
-import android.companion.BluetoothDeviceFilter;
-import android.companion.CompanionDeviceManager;
-import android.content.BroadcastReceiver;
-import android.content.Context;
 import android.content.Intent;
-import android.content.IntentFilter;
-import android.content.IntentSender;
-import android.content.pm.PackageManager;
-import android.content.res.Resources;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Canvas;
+
 import android.graphics.Color;
-import android.graphics.ColorFilter;
-import android.graphics.ColorMatrix;
-import android.graphics.ColorMatrixColorFilter;
-import android.graphics.Paint;
-import android.graphics.PixelFormat;
 import android.graphics.PorterDuff;
-import android.graphics.Rect;
-import android.graphics.RectF;
-import android.graphics.drawable.BitmapDrawable;
-import android.graphics.drawable.ColorDrawable;
-import android.graphics.drawable.Drawable;
+import android.graphics.PorterDuffColorFilter;
 import android.graphics.drawable.GradientDrawable;
-import android.graphics.drawable.ShapeDrawable;
-import android.graphics.drawable.shapes.RectShape;
-import android.os.Build;
+
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Looper;
-import android.os.ParcelUuid;
-import android.os.SystemClock;
-import android.util.Base64;
 import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.widget.BaseAdapter;
+
 import android.widget.ImageButton;
 import android.widget.ImageView;
-import android.widget.SeekBar;
-import android.widget.Toast;
 
-//import com.clj.fastble.BleManager;
-//import com.clj.fastble.callback.BleGattCallback;
-//import com.clj.fastble.callback.BleScanCallback;
-//import com.clj.fastble.data.BleDevice;
-//import com.clj.fastble.exception.BleException;
-//import com.clj.fastble.scan.BleScanRuleConfig;
-//import com.google.android.gms.location.LocationServices;
 import com.polidea.rxandroidble2.LogConstants;
 import com.polidea.rxandroidble2.LogOptions;
 import com.polidea.rxandroidble2.RxBleClient;
-import com.polidea.rxandroidble2.RxBleConnection;
 import com.polidea.rxandroidble2.RxBleDevice;
-import com.polidea.rxandroidble2.scan.ScanFilter;
-import com.polidea.rxandroidble2.scan.ScanSettings;
+
 import com.rtugeek.android.colorseekbar.ColorSeekBar;
-//import com.welie.blessed.BluetoothCentralManager;
-//import com.welie.blessed.BluetoothCentralManagerCallback;
-//import com.welie.blessed.BluetoothPeripheral;
-//import com.welie.blessed.BluetoothPeripheralCallback;
-//import com.welie.blessed.ConnectionPriority;
-import io.reactivex.Observable;
-import io.reactivex.disposables.CompositeDisposable;
-import io.reactivex.disposables.Disposable;
-import io.reactivex.subjects.PublishSubject;
 
-import java.util.ArrayList;
+import io.reactivex.disposables.Disposable;
+
 import java.util.Arrays;
-
-import io.reactivex.disposables.CompositeDisposable;
-import io.reactivex.disposables.Disposable;
-
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-import java.util.Timer;
-import java.util.TimerTask;
 import java.util.UUID;
-import java.util.concurrent.TimeUnit;
-import java.util.regex.Pattern;
-
-import io.reactivex.Observable;
-import io.reactivex.disposables.Disposable;
 
 public class MainActivity extends AppCompatActivity {
+    /* Instance Variables */
+    private ColorSeekBar colorSeekBar;
+    private ColorSeekBar saturation_seekbar;
+    private ColorSeekBar brightness_seekbar;
+    private ImageButton button;
 
-    private static final int REQUEST_ENABLE_BT = 1;
-    private static final int ACCESS_COARSE_LOCATION_REQUEST = 1;
-    ColorSeekBar colorSeekBar;
-    ColorSeekBar saturation_seekbar;
-    ColorSeekBar brightness_seekbar;
-    ImageButton button;
-    private int brightness_position = 100;
-    private int color_position = 100;
-    private int saturation_position = 100;
-    boolean isOn = true;
-    ImageView imView;
-    private final String lampiMacAddress = "B8:27:EB:CB:AF:1F";
+    private boolean isOn = true;
     private GradientDrawable rectangle;
-    private RxBleDevice myLampi;
 
     Disposable temp;
     private final UUID onOffUUID = UUID.fromString("0004A7D3-D8A4-4FEA-8174-1736E808C066");
     private final UUID brightnessUUID = UUID.fromString("0003A7D3-D8A4-4FEA-8174-1736E808C066");
     private final UUID hsvUUID = UUID.fromString("0002A7D3-D8A4-4FEA-8174-1736E808C066");
+    private final String lampiMacAddress = "B8:27:EB:CB:AF:1F";
 
+    private int brightness_position = 100;
+    private int color_position = 100;
+    private int saturation_position = 100;
 
-    private byte[] getByteValues(String bar) {
+    /**
+     * Function to read a specific characteristic of the lamp
+     * @param bar: The characteristic I wish to read (hsv, brightness, onOff)
+     */
+    private void readFromLamp(String bar) {
         RxBleDevice myLampi = getMyLampi(lampiMacAddress);
 
+        // Figure out which UUID I wish to read from
         UUID tempUUID = onOffUUID;
         switch (bar) {
             case "hsv":
@@ -175,43 +76,44 @@ public class MainActivity extends AppCompatActivity {
         }
 
         UUID toReadUUID = tempUUID;
-        final byte[][] output = {new byte[]{50}};
 
+        // Reading from the Lamp
         temp = myLampi.establishConnection(false)
                 .flatMapSingle(rxBleConnection -> rxBleConnection.readCharacteristic(toReadUUID))
                 .subscribe(
                         characteristicValue -> {
-                            // Read characteristic value.
-                            output[0] = characteristicValue;
-
-//                           temp.dispose();
+                            // Update seekbar sliders accordingly
                             if (bar.equals("brightness")) {
-                                brightness_position = byteToInt(output[0][0]);
+                                brightness_position = byteToInt(characteristicValue[0]);
                                 brightness_seekbar.setPosition(brightness_position, 0);
-                            }
-                            if (bar.equals("hsv")) {
-                                Log.d("Result", "SAT POS : " + saturation_position);
-                                Log.d("Result", "ACTUAL HUE : " + output[0][0] + " Actual: SATURATION " + output[0][1]);
-                                color_position = byteToInt(output[0][0]);
+                            } else if (bar.equals("hsv")) {
+                                color_position = byteToInt(characteristicValue[0]);
                                 colorSeekBar.setPosition(color_position, 0);
-                                saturation_position = byteToInt(output[0][1]);
+
+                                saturation_position = byteToInt(characteristicValue[1]);
                                 saturation_seekbar.setPosition(saturation_position, 0);
                             }
 
+                            // Update button and rectangle background colors
+                            button.setColorFilter(saturation_seekbar.getColor());
                             rectangle.setColorFilter(applyLightness(100 - brightness_position));
                         },
                         throwable -> {
                             // Handle an error here.
-                            Log.d("Result", "READING" + " " + throwable.toString());
+                            Log.d("Reading", "ERROR READING:" + " " + throwable.toString());
                         });
-        return output[0];
     }
 
-
+    /**
+     * Function to write to a characteristic of the lamp
+     * @param bar: The type of characteristic I wish to write to (hsv, brightness, onOff)
+     * @param bytesToWrite: The byte array of data I wish to write to the lamp
+     */
     private void writeToLamp(String bar, byte[] bytesToWrite) {
         RxBleDevice myLampi = getMyLampi(lampiMacAddress);
         UUID toWriteUUID;
 
+        // Figure out which UUID I wish to write to
         switch (bar) {
             case "hsv":
                 toWriteUUID = hsvUUID;
@@ -230,29 +132,41 @@ public class MainActivity extends AppCompatActivity {
                 return;
         }
 
+        // Write to the Lamp
         temp = myLampi.establishConnection(false)
                 .flatMapSingle(rxBleConnection -> rxBleConnection.writeCharacteristic(toWriteUUID, bytesToWrite))
                 .subscribe(
                         characteristicValue -> {
                             // Characteristic value confirmed.
-                            Log.d("Result", "CHANGED: " + characteristicValue.toString());
+                            Log.d("Result", "CHANGED: " + Arrays.toString(characteristicValue));
                         },
                         throwable -> {
                             // Handle an error here.
                             Log.d("Result", "ERROR : " + throwable.toString());
                         }
                 );
-
     }
 
+    // Function to get Lampi device with MAC address "lampiMac"
+
+    /**
+     * Function to get the RxBleDevice with MAC address lampiMAC
+     * @param lampiMAC: The MAC address of the device I am trying to connect to
+     * @return: The device I am trying to connect to
+     */
     private RxBleDevice getMyLampi(String lampiMAC) {
         RxBleClient rxBleClient = RxBleClient.create(this);
 
-        // Get my device
-        RxBleDevice device = rxBleClient.getBleDevice(lampiMAC);
-        return device;
+        return rxBleClient.getBleDevice(lampiMAC);
     }
 
+    //
+
+    /**
+     * Function for reading from Lamp: Used to convert Byte[] read from Lamp to change the seekbars of the app UI
+     * @param input: The input byte array read from the lamp
+     * @return: The integer value representing where the seekbar should be positioned
+     */
     private int byteToInt(byte input) {
         int answer;
         if (input < 0) {
@@ -264,19 +178,20 @@ public class MainActivity extends AppCompatActivity {
         return answer;
     }
 
+    /**
+     * Function for writing to Lamp: Used to convert integer of color/saturation/brightness_seekbar to byte array (for writing to Lamp)
+     * @param colorBarPosition: The seekbar position (0-100)
+     * @return: The byte array equivalent value for the seekbar on the Lampi
+     */
     private byte[] getInputByte(int colorBarPosition) {
         double answer;
 
-        // colorBarPosition =>
-        // Integer: 0 -> 50 -> 51 -> 100
-        // Byte: 0 -> 128 -> -127 -> -1
-        if (colorBarPosition < 50) { // 0->127
+        if (colorBarPosition < 50) { // 0 => 127
             answer = (127.0 / 50.0) * (double) colorBarPosition;
-        } else {
+        } else { // -128 => -1
             answer = ((127.0 / 50.0) * (double) colorBarPosition) - 255;
         }
 
-        Log.d("BRIGHTNESS", answer + "");
         return new byte[]{(byte) answer};
     }
 
@@ -284,13 +199,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Instantiate button, seekbars, and rectangle background
         button = (ImageButton) this.findViewById(R.id.imageButton);
-        Log.d("Here", "SeekBarCreation");
-
-
-        imView = findViewById(R.id.rectangleBackground);
+        button.setColorFilter(Color.WHITE);
+        ImageView imView = findViewById(R.id.rectangleBackground);
         rectangle = (GradientDrawable) imView.getBackground();
 
+        // Create the seekbars
+        seekBarCreation();
+
+        // Bluetooth - Logging for Testing Purposes
         RxBleClient.updateLogOptions(new LogOptions.Builder()
                 .setLogLevel(LogConstants.INFO)
                 .setMacAddressLogSetting(LogConstants.MAC_ADDRESS_FULL)
@@ -304,80 +223,53 @@ public class MainActivity extends AppCompatActivity {
         int REQUEST_ENABLE_BT = 1;
         this.startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
 
-        myLampi = getMyLampi(lampiMacAddress);
+        // Sync the app UI with lamp
+        readFromLamp("hsv");
+        readFromLamp("brightness");
 
-        seekBarCreation();
-//
+        // Constantly read the KIVY UI for updates (to change app sliders accordingly)
 //        new Timer().scheduleAtFixedRate(new TimerTask() {
 //            @Override
 //            public void run() {
-//                getByteValues("hsv");
-//                getByteValues("brightness");
+//                readFromLamp("hsv");
+//                readFromLamp("brightness");
 //
 ////                temp.dispose();
 //            }
 //        }, 0, 3000);
 
 
-        button.bringToFront();
-        button.setOnClickListener(v -> {
-            isOn = !isOn;
-            Log.d("Button", Boolean.toString(button.isSelected()));
-
-//                button.set
-            if (isOn) {
-                writeToLamp("onOff", new byte[]{1});
-                button.setColorFilter(saturation_seekbar.getColor());
-            } else {
-                writeToLamp("onOff", new byte[]{0});
-                button.setColorFilter(Color.WHITE);
-            }
-        });
+        isOn = true;
     }
 
 
-    // Every second, run
-
-
-//
-//    @Override
-//    protected void onResume() {
-//        handler.postDelayed(runnable = new Runnable() {
-//            public void run() {
-//                handler.postDelayed(runnable, delay);
-//                // Constantly read the KIVY UI and write to Android
-////                byte[] byteToSetBrightness = getByteValues("brightness");
-//
-////                writeToLamp("brightness", getByteValues("brightness"));
-//            }
-//        }, 5000);
-//        super.onResume();
-//    }
-
+    /**
+     * Function to create the seekbars and button for the app UI
+     */
     public void seekBarCreation() {
+        // Instantiate variables
         colorSeekBar = findViewById(R.id.color_seek_bar);
         saturation_seekbar = findViewById((R.id.saturation_seekbar));
         brightness_seekbar = findViewById((R.id.brightness_seekbar));
 
         colorSeekBar.setOnColorChangeListener((colorBarPosition, alphaBarPosition, color) -> {
+            // On color slider change, change the slider gradient color of saturation slider
             rectangle.setColor(saturation_seekbar.getColor());
             saturation_seekbar.setColorSeeds(new int[]{Color.WHITE, colorSeekBar.getColor()});
             if (isNear(colorBarPosition, color_position)) {
                 return;
             }
 
-
-
             color_position = colorBarPosition;
             byte[] toWrite = new byte[3];
-            toWrite[0] = getInputByte(color_position)[0];
-            toWrite[1] = getInputByte(saturation_position)[0];
-            toWrite[2] = -1;
+            toWrite[0] = getInputByte(color_position)[0]; // hue
+            toWrite[1] = getInputByte(saturation_position)[0]; // saturation
+            toWrite[2] = -1; // v
 
-            writeToLamp("hsv", toWrite);
+            writeToLamp("hsv", toWrite); // Write the changes to the lamp
 
             if (isOn) {
-                button.setColorFilter(saturation_seekbar.getColor());
+                button.setColorFilter(saturation_seekbar.getColor()); // If the lamp is on, change the color of the app power button
             }
         });
 
@@ -389,20 +281,18 @@ public class MainActivity extends AppCompatActivity {
 
             saturation_position = colorBarPosition;
             byte[] toWrite = new byte[3];
-            toWrite[0] = getInputByte(color_position)[0];
-            toWrite[1] = getInputByte(saturation_position)[0];
-            toWrite[2] = -1;
+            toWrite[0] = getInputByte(color_position)[0]; // hue
+            toWrite[1] = getInputByte(saturation_position)[0]; // saturation
+            toWrite[2] = -1; //v
 
-//            byte[] readingByte = getByteValues("hsv");
-            writeToLamp("hsv", toWrite);
-
+            writeToLamp("hsv", toWrite); // Write the changes to the lamp
 
             if (isOn) {
-                button.setColorFilter(saturation_seekbar.getColor());
+                button.setColorFilter(saturation_seekbar.getColor()); // If the lamp is on, change the color of the app power button
             }
         });
 
-
+        // Listener for Brightness Seekbar
         brightness_seekbar.setOnColorChangeListener((colorBarPosition, alphaBarPosition, color) -> {
             if (isNear(colorBarPosition, brightness_position)) {
                 return;
@@ -414,9 +304,23 @@ public class MainActivity extends AppCompatActivity {
             rectangle.setColor(saturation_seekbar.getColor());
             rectangle.setColorFilter(applyLightness(100 - brightness_position));
         });
+
+        // Creation of Button
+        button.bringToFront();
+        button.setOnClickListener(v -> {
+            isOn = !isOn;
+            Log.d("onOff", isOn + "");
+            if (isOn) {
+                writeToLamp("onOff", new byte[]{1});
+                button.setColorFilter(saturation_seekbar.getColor());
+            } else {
+                writeToLamp("onOff", new byte[]{0});
+                button.setColorFilter(Color.WHITE);
+            }
+        });
     }
 
-
+    // To prevent bluetooth library from disconnecting due to rapid writes
     private boolean isNear(int colorBarPosition, int position) {
         return (position == colorBarPosition
                 || position == colorBarPosition - 1
@@ -425,6 +329,7 @@ public class MainActivity extends AppCompatActivity {
                 || position == colorBarPosition + 2);
     }
 
+    // Change the brightness of the app
     private PorterDuffColorFilter applyLightness(int progress) {
         return new PorterDuffColorFilter(Color.argb(progress * 255 / 100, 255, 255, 255), PorterDuff.Mode.SRC_OVER);
     }
